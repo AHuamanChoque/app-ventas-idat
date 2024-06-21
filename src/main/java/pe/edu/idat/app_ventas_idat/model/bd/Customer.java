d05c569da5cc69a24bc0b34475d840395a41e50d
AHuamanChoque <adriansan734@gmail.com>
2024-06-20 20:03:21 -0500

    Avance ventas

--- src/main/java/pe/edu/idat/app_ventas_idat/model/bd/Customer.java
+++ src/main/java/pe/edu/idat/app_ventas_idat/model/bd/Customer.java
@@ -1,4 +1,26 @@
 package pe.edu.idat.app_ventas_idat.model.bd;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table (name = "customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer customerid;
+    private String address;
+    private String city;
+    private String companyname;
+    private String contacname;
+    private String contactname;
+    private String contacttitle;
+    private String country;
+    private String fax;
+    private String phone;
+    private String postalcode;
+    private String region;
 }
--- src/main/java/pe/edu/idat/app_ventas_idat/model/bd/Order.java
+++ src/main/java/pe/edu/idat/app_ventas_idat/model/bd/Order.java
@@ -1,4 +1,28 @@
 package pe.edu.idat.app_ventas_idat.model.bd;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer orderid;
+    private Double freight;
+    private Date orderdate;
+    private Date requireddate;
+    private String shipaddress;
+    private String shipcity;
+    private String shipcountry;
+    private String shipname;
+    private Date shipdate;
+    private String shippostalcode;
+    private String shipregion;
+    private Integer shipvia;
+
 }
